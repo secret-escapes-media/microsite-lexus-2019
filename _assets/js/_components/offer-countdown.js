84e001589fcbb09fd9fb8938796c0c0b6c2b3317
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2019-04-17 09:22:37 +0100

    correct countdown day/days reference

--- _assets/js/_components/offer-countdown.js
+++ _assets/js/_components/offer-countdown.js
@@ -18,7 +18,7 @@ $('.js-offer-expires').each(function() {
       $(this).html(event.strftime('Slutar <strong>idag</strong>'));
     } else {
       // there are days left, outputs with either day or days
-      $(this).html(event.strftime('Slutar om <strong>%-D %!d:dag,dagar;</strong>'));
+      $(this).html(event.strftime('Slutar om <strong>%-D %!D:dag,dagar;</strong>'));
     }
   });
 });
